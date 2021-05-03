eba5c99175df63eb3a194c63af1c4e2a3e78b170
Criselpro2007 <criselpro2007@gmail.com>
2021-05-03 16:39:23 -0400

    Update say.js

--- commands/otros/say.js
+++ commands/otros/say.js
@@ -3,13 +3,10 @@ module.exports = { //exportamos el nombre, la descripción y la función execute
     description: 'el bot dice lo que quieras', //la descripción (opcional)
     execute(client, message, args) { //ejecutamos...
         
-    if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send ('Necesito permisos de **GESTIONAR MENSAJES**')
-    if(!message.guild) return;
-    
-if(message.content.includes('@')) return message.channel.send('Me niego a escribir eso!')
+        
 
-  if(!args) return msg.channel.send(`Debes escribir un mensaje para enviar.`);
-  message.delete();
+        if(!args) return message.channel.send(`debe escribir un mensaje a enviar.`);
+        message.delete();
         message.channel.send(args.slice(1).join(" ")) //pong!
     }
 };
