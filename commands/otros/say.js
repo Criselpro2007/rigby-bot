4c13d8af123c1271a155c3386c11e9809ff8cba4
Criselpro2007 <criselpro2007@gmail.com>
2021-05-03 16:37:05 -0400

    Update say.js

--- commands/otros/say.js
+++ commands/otros/say.js
@@ -5,10 +5,11 @@ module.exports = { //exportamos el nombre, la descripción y la función execute
         
     if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send ('Necesito permisos de **GESTIONAR MENSAJES**')
     if(!message.guild) return;
+    
 if(message.content.includes('@')) return message.channel.send('Me niego a escribir eso!')
 
   if(!args) return msg.channel.send(`Debes escribir un mensaje para enviar.`);
-  message.delete()
+  message.delete();
         message.channel.send(args.slice(1).join(" ")) //pong!
     }
 };
