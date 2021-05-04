e6508a5ca35529ea5c3a7d3ff7f7b1abad035bc6
Criselpro2007 <criselpro2007@gmail.com>
2021-05-04 16:47:31 -0400

    Update ping.js

--- commands/otros/ping.js
+++ commands/otros/ping.js
@@ -8,7 +8,7 @@ let ping = Math.floor(message.client.ping);
     message.channel.send(":ping_pong: Pong!")
     .then(msg => {
         
-        msg.edit(`:incoming_envelope: Ping Mensajes: \`${Math.floor(msg.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
+        msg.edit(`:ping_pong: Pong! | :incoming_envelope: Ping Mensajes: \`${Math.floor(msg.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
 })
     }
 };
