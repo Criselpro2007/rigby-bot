b4b6fa6092c64483daf440c112c721b1ab97971b
Criselpro2007 <criselpro2007@gmail.com>
2021-05-04 16:01:38 -0400

    Update ping.js

--- commands/otros/ping.js
+++ commands/otros/ping.js
@@ -2,11 +2,13 @@ module.exports = { //exportamos el nombre, la descripción y la función execute
     name: 'ping', //el nombre del comando
     description: 'El mejor comando!', //la descripción (opcional)
     execute(client, message, args) { //ejecutamos...!
-    }
-};
+    
+
 let ping = Math.floor(message.client.ping);
     message.channel.send(":ping_pong: Pong!")
-    .then(m => {
+    .then(msg => {
         
-        m.edit(`:incoming_envelope: Ping Mensajes: \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
-});
+        msg.edit(`:incoming_envelope: Ping Mensajes: \`${Math.floor(msg.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
+})
+    }
+};
