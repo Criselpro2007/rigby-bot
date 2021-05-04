d67e3e34e9d447856f361b2e59c0a244e86888f0
Criselpro2007 <criselpro2007@gmail.com>
2021-05-04 08:32:47 -0400

    server command handler

--- index.js
+++ index.js
@@ -259,24 +259,6 @@ client.on("message", async message => {
           return
 
         })
-      
-//Server
-      }if (command === 'server') {
-        var server = message.guild;
-
-        const embed = new Discord.MessageEmbed()
-        .setThumbnail(server.iconURL())
-        .setAuthor(server.name, server.iconURL())
-        .addField('ID', server.id, true)
-        .addField('Region', server.region, true)
-        .addField('Creado el', server.joinedAt.toDateString(), true)
-        .addField('Dueño del Servidor', server.owner.user.tag+' ('+server.owner.user.id +')', true)
-        .addField('Miembros', server.memberCount, true)
-        .addField('Roles', server.roles.size, true)
-        .setColor(0x66b3ff)
-        
-        message.channel.send(embed); 
-        return
 
 /////MODERACIÓN
 //Ban
