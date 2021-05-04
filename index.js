1843e802fc54b7b1995f88d58a1a167fb4f929f2
Criselpro2007 <criselpro2007@gmail.com>
2021-05-04 13:49:49 -0400

    Xd

--- index.js
+++ index.js
@@ -721,26 +721,5 @@ client.on('messageDelete', message =>{
   })
 })
 
-client.on('message', message => {
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + 'snipe'))
-  const channel = message.mentions.channel.first() || message.channel;
-
-  const msg = client.snipes.get(channel.id)
-  if(!msg){
-      message.channel.send('No se ha borrado ningún mensaje')
-  }else {
-      const embed = new Discord.MessageEmbed()
-  
-      .setTitle('Snipe')
-      .setAuthor(`Mensaje escrito por: ${msg.delete.tag}`, msg.delte.displayAvatarURL())
-      .addField('Canal', `<#${msg.canal.id}>`)
-      .setDescription(msg.content)
-      .setColor('RANDOM')
-  
-      message.channel.send(embed);
-  }
-});
-
 
 client.login(process.env.token);
