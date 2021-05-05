54b806d86e55337ef25416eee2311055e9b4b070
Criselpro2007 <criselpro2007@gmail.com>
2021-05-04 22:04:57 -0400

    Update snipe.js

--- commands/moderación/snipe.js
+++ commands/moderación/snipe.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
-module.exports = { //exportamos el nombre, la descripción y la función execute
-    name: 'snipe', //el nombre del comando
-    description: 'Comando snipe', //la descripción (opcional)
-    execute(client, message, args) { //ejecutamos...
+module.exports = { 
+    name: 'snipe', 
+    description: 'Comando snipe', 
+    execute(client, message, args) { 
 const channel = message.mentions.channel.first() || message.channel;
 
 const msg = client.snipes.get(channel.id)
